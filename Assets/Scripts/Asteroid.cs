21d9aab0c524962e719274d775282082b23ff945
OzkanCetin <ozkancetin@posta.mu.edu.tr>
2024-05-27 11:07:12 +0300

    asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -8,6 +8,7 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField] private Transform TargetPosition;
     [SerializeField] private float speed;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,6 +17,15 @@ public class Asteroid : MonoBehaviour
             Vector2 currentPosition = transform.position;
             Vector2 targetPosition = new Vector2(TargetPosition.position.x,TargetPosition.position.y);
             transform.position = Vector2.MoveTowards(currentPosition,targetPosition,speed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        print(other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SendMessage("death");
         }
     }
 }
